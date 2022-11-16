6adb420ca682a9bfefc37c940e851df4c4a3f499
muhammadali2858 <ali.waris2858@gmail.com>
2022-11-16 13:14:07 -0800

    Initial commit

---
+++ js-basic.js
@@ -0,0 +1,26 @@
+let =prompt("hi ali");
+//document.write("Welcome to javascript");
+
+/*
+let SubEnglighClass6th=70;
+let SubScienceClass6th=85;
+let SubUrduClass6th=96;
+let SubMathClass6th=60;
+let Total=SubEnglighClass6th+SubScienceClass6th+SubUrduClass6th+SubMathClass6th;
+
+document.write("English" + '=' +  SubEnglighClass6th);
+document.write("Sceience" + '=' +  SubScienceClass6th);
+document.write("Urdu" + '=' +  SubUrduClass6th);
+document.write("Math" + '=' +  SubMathClass6th);
+document.write("Total" + '=' + Total);
+*/
+/*
+let a=5;
+let b=4;
+let c=a*b;
+document.write(c);
+*/
+let a=550;
+let b=300;
+per= b*100/a;
+document.write(per);
